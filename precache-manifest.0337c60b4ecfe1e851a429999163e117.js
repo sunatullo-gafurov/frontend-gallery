9091fd2d05dcbb3863adcb73e6cc2732f8196552
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-19 09:22:03 +0000

    Deploy sunatullo-gafurov/frontend-gallery to github.com/sunatullo-gafurov/frontend-gallery.git:gh-pages

---
+++ precache-manifest.0337c60b4ecfe1e851a429999163e117.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bba2a8fb33dd749c6c0df0825e26a64",
+    "url": "/frontend-gallery/index.html"
+  },
+  {
+    "revision": "5fa6793161995025459f",
+    "url": "/frontend-gallery/static/css/main.acdfe0ae.chunk.css"
+  },
+  {
+    "revision": "c339520fc0bf7bfa7cf1",
+    "url": "/frontend-gallery/static/js/2.ebeee30d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/frontend-gallery/static/js/2.ebeee30d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa6793161995025459f",
+    "url": "/frontend-gallery/static/js/main.d323effb.chunk.js"
+  },
+  {
+    "revision": "a58d9917aa1e10aceebc",
+    "url": "/frontend-gallery/static/js/runtime-main.5f0eb6d3.js"
+  },
+  {
+    "revision": "abf1a95f1c1c42e8148b095152f7c77f",
+    "url": "/frontend-gallery/static/media/pngtree.abf1a95f.jpg"
+  }
+]);
